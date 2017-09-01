51a4cdf33d3444312eff945ff43c53ea279b6213
Olivier Butler <olivier.butler@gmail.com>
2017-08-31 20:29:54 -0700

    Routing to portal app completed, basic template working

--- apps/app_portal/views.py
+++ apps/app_portal/views.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def test(req):
+    print """
+    Boob ya!
+    """
+
+def render_portal(req):
+    return render(req, 'app_portal/index.html')
--- project_stack/settings.py
+++ project_stack/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.app_portal',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
