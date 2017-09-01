c96b6be85e8bf9c9ab7642fc84ba098f6fe5d395
Olivier Butler <olivier.butler@gmail.com>
2017-09-01 01:15:07 -0700

    Email in model is as it needs to be. Note that one can manually create users with invalid emails

--- apps/app_portal/models.py
+++ apps/app_portal/models.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 from django.db import models
 from model_fields import PasswordField
+from django.core.validators import EmailValidator
 
 from model_managers import UserManager
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField()
+    email = models.EmailField(max_length=254, blank=False, unique=True)
     password = PasswordField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
