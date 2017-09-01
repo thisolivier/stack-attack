1e7c9e0c33e756825e4fbcad2969abbfaf4227ae
Olivier Butler <olivier.butler@gmail.com>
2017-09-01 00:13:04 -0700

    NOPE not going there

--- apps/app_portal/models.py
+++ apps/app_portal/models.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-# from model_fields import PasswordField
-
-from model_managers import UserManager
 
 # Create your models here.
 class User(models.Model):
