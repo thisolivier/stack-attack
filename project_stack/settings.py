6ebf2b65e0f786ec733c6e919bbbde59442f0720
Olivier Butler <olivier.butler@gmail.com>
2017-09-01 02:23:21 -0700

    Bootstrap4 now installed and rendering my forms

--- project_stack/settings.py
+++ project_stack/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.app_portal',
-    'bootstrap3',
+    'crispy_forms',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
